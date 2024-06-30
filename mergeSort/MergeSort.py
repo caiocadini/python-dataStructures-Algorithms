7b545f2ea4875ff937b267518ce77dc39df18bfa
Caio Cadini <cadinigithub@gmail.com>
2024-06-30 20:42:50 -0300

    Update merge Sort

--- mergeSort/MergeSort.py
+++ mergeSort/MergeSort.py
@@ -1,39 +1,39 @@
 def mergeSort(array):
-    if len(array) > 1:
-        # Divindindo array no meio
-        left_array = array[:len(array)//2] 
-        right_array = array[len(array)//2:]
+    if len(array) <= 1:
+        return array
+    # Divindindo array no meio
+    left_array = array[:len(array)//2] 
+    right_array = array[len(array)//2:]
 
-        #Dividir o array recursivamente
-        mergeSort(left_array)
-        mergeSort(right_array)
+    #Dividir o array recursivamente
+    left_array = mergeSort(left_array)
+    right_array = mergeSort(right_array)
 
-        i = j = k = 0
-        #Ordenando por comparação entre partes menores
-        while k < len(left_array) and j < len(right_array):
-            if(left_array[k] < right_array[j]):
-                array[i] = left_array[k]
-                k += 1
-            else:
-                array[i] = right_array[j]
-                j += 1
-
-            i += 1
-        # Colocar elementos restantes em cada array
-        while k < len(left_array):
-            array[i] = left_array[k]
+    sorted_array = []
+    j = k = 0
+    #Ordenando por comparação entre partes menores
+    while k < len(left_array) and j < len(right_array):
+        if(left_array[k] < right_array[j]):
+            sorted_array.append(left_array[k])
             k += 1
-            i += 1
-        
-        while j < len(right_array):
-            array[i] = right_array[j]
+        else:
+            sorted_array.append(right_array[j])
             j += 1
-            i += 1
+    # Colocar elementos restantes em cada array
+    while k < len(left_array):
+        sorted_array.append(left_array[k])
+        k += 1
+    
+    while j < len(right_array):
+        sorted_array.append(right_array[j])
+        j += 1
+    
+    return sorted_array
         
 
 arr = [38, 27, 43, 3, 9, 82, 10]
 print("Original Array:", arr)
 
-mergeSort(arr)
-print("Sorted Array:", arr)
+result = mergeSort(arr)
+print("Sorted Array:", result)
     
